ddbba749dc2efa143a5837c77bf7f254ccb3f209
文银龙 <lywen@ly.local>
2017-07-14 14:55:56 +0800

    init module

--- fes_celery/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 12 15:29:36 2017
-celery[redis]
-@author: lywen
-"""
-
--- spyder/spyder_tushare.py
+++ spyder/spyder_tushare.py
@@ -2,8 +2,24 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jul 14 14:12:38 2017
-
+运用tushare 获取
 @author: lywen
+历史行情数据
+复权历史数据
+实时行情数据
+历史分笔数据
+实时报价数据
+当日历史分笔
+大盘指数列表
+大单交易数据
 """
 import tushare
+
+class SymbolMdm(object):
+    """
+    股票基础数据
+    """
+    def __init__(self):
+        pass
+    
 
